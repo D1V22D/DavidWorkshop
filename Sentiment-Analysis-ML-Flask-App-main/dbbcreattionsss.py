504a646c129568be33c22f0ca2de63dd6d6c3751
D1V22D <127064600+D1V22D@users.noreply.github.com>
2024-07-05 21:02:05 +0530

    Add files via upload

---
+++ Sentiment-Analysis-ML-Flask-App-main/dbbcreattionsss.py
@@ -0,0 +1,15 @@
+import mysql.connector
+#create the connection object
+myconn = mysql.connector.connect(host = "localhost", user = "root",passwd = "Admin@123")
+#creating the cursor object
+cur = myconn.cursor()
+try:
+#creating a new database
+    cur.execute("create database Python")
+#getting the list of all the databases which will now include the new database PythonDB
+    dbs = cur.execute("show databases")
+except:
+    myconn.rollback()
+for x in cur:
+    print(x)
+myconn.close()
